5d3d69ece998a80c49f4502dc41d4ed48ecd49ee
CilekciGs <75041882+CilekciGs@users.noreply.github.com>
2022-05-04 14:05:31 +0300

    Trying to run recieve and send at the same time
    Trying to stop chess-like chatting

--- server.py
+++ server.py
@@ -2,19 +2,17 @@
 
 import socket
 
-
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+s.listen(0)
 
 conn, addr = s.accept()
-print ('Connection address:'), addr
+print(conn,"  addr   ",addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
     print("received data:", data)
     conn.send(data)  # echo
-conn.close()
